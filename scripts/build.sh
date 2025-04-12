3de3096fd6adbc895408603f5b29c6b911b7a19e
Ritesh Tiwary <37486223+ritesh-tiwary@users.noreply.github.com>
2025-04-12 08:42:52 +0000

    🎉 Initial commit

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 export DOCKER_BUILDKIT=1
 export COMPOSE_BAKE=true
 
-docker-compose build  --no-cache --profile ignore-me down --file docker-compose.yml
+docker-compose build  --no-cache --profile ignore-me backend down --file docker-compose.yml
 docker-compose up --detach
