db8b2bc587a5466e7af1a174a502f251b8ca2453
Ritesh Tiwary <37486223+ritesh-tiwary@users.noreply.github.com>
2025-04-12 10:19:06 +0000

    🎉 Initial commit

--- app/worker/tasks.py
+++ app/worker/tasks.py
@@ -5,6 +5,31 @@ from celery.exceptions import MaxRetriesExceededError
 
 logger = Logger(__name__)
 
+@celery.task(name='app.worker.tasks.parse', bind=True, max_retries=3)
+def parse_data(self, bytes_obj: bytes):
+    """
+    Parse data
+    """
+    try:
+        logger.info(len(bytes_obj))
+    except Exception as e:
+        logger.error(f"Error occurred: {e}")
+        raise self.retry(exc=e, countdown=5)
+
+
+@celery.task(name='app.worker.tasks.load', bind=True, max_retries=3)
+def load_data(self):
+    """
+    Load data
+    """
+    try:
+        logger.info('Data loader started')
+        # task = Task(task_id=add_with_retry.request.id, status="completed", result=result)
+        # await task.insert()
+    except Exception as e:
+        logger.error(f"Error occurred: {e}")
+        raise self.retry(exc=e, countdown=5)  # Retry after 5 seconds
+
 @celery.task(name='app.worker.tasks.add', bind=True, max_retries=3)
 def add_with_retry(self, x, y):
     """
@@ -12,8 +37,6 @@ def add_with_retry(self, x, y):
     """
     try:
         result = x + y
-        # task = Task(task_id=add_with_retry.request.id, status="completed", result=result)
-        # await task.insert()
         return result
     except Exception as e:
         logger.error(f"Error occurred: {e}")
