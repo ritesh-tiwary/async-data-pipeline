af98b0d32ad4ebb18c2ec6a0e7fcd6fa0c8576f5
Ritesh Tiwary <37486223+ritesh-tiwary@users.noreply.github.com>
2025-05-01 15:18:25 +0000

    🎉 Initial commit

--- app/core/database.py
+++ app/core/database.py
@@ -11,82 +11,87 @@ class Databse(Base):
 
     async def save_dlq_to_file(self, query, record, error_message):
         """Saves failed records to a JSON file as a backup."""
-        failed_record = {"query": query, "record": record, "error": error_message}
+        failed_record = {"query": query.strip(), "record": record, "error": error_message}
         try:
             with open(self.DLQ_FILE, "a") as f:
-                f.write(orjson.dumps(failed_record).decode("utf-8") + "\n")
-            self.logging.info(f"Saved to DLQ file: {failed_record}")
+                f.write(orjson.dumps(failed_record).decode("utf-8") + ",")
+            self.logger.info(f"Saved to DLQ file: {failed_record}")
         except Exception as e:
-            self.logging.error(f"Failed to save to DLQ file: {e}")
+            self.logger.error(f"Failed to save to DLQ file: {e}")
 
-    async def send_to_dlq(self, cursor, query, record, error_message):
+    async def send_to_dlq(self, connection, query, record, error_message):
         """Inserts failed record into the DLQ table in Postgres."""
         try:
-            await cursor.execute("INSERT INTO failed_inserts (query, record, error_message) VALUES (?, ?, ?)", 
+            await connection.execute("INSERT INTO failed_inserts (query, record, error_message) VALUES ($1, $2, $3)", 
                                 (query, record, error_message))
-            self.logging.info(f"Sent to DLQ: {record} - Error: {error_message}")
+            self.logger.info(f"Sent to DLQ: {record} - Error: {error_message}")
         except Exception as e:
-            self.logging.error(f"Failed to insert into DLQ: {record} - Error: {e}")
+            self.logger.error(f"Failed to insert into DLQ: {record} - Error: {e}")
             await self.save_dlq_to_file(query, record, error_message)  # Fallback to JSON file
 
-    async def insert_batch_with_retry(self, cursor, query, batch, retries=3):
+    async def insert_batch_with_retry(self, connection, query, batch, batch_id, consumer_id, retries=3):
         """Tries batch inserting data with retries. If all fail, moves to DLQ."""
         for attempt in range(1, retries + 1):
             try:
-                await cursor.executemany(query, batch)
-                self.logging.info(f"Batch inserted {len(batch)} records")
+                await connection.executemany(query, batch)
+                self.logger.info(f"Batch-{batch_id} inserted {len(batch)} records")
                 return True
             except Exception as e:
-                self.logging.warning(f"Batch insert attempt {attempt} failed: {e}")
+                ex = str(e)
+                self.logger.error(f"Batch-{batch_id} insert attempt {attempt} failed: {ex}")
                 await asyncio.sleep(2**attempt)
 
-        self.logging.error(f"Failed to insert batch after {retries} attempts: {batch}")
+        self.logger.error(f"Consumer-{consumer_id} failed to insert Batch-{batch_id} after {retries} attempts")
         for record in batch:
-            await self.send_to_dlq(cursor, query, str(record), str(e))
-        return False
+            await self.send_to_dlq(connection, query, str(record), ex)
 
     async def consumer(self, queue, pool, query, consumer_id, batch_size):
         """Consumes producer data from the queue and inserts it into Postgres with DLQ handling."""
         async with pool.acquire() as conn:
-            async with conn.cursor() as cursor:
-                batch = []
-                while True:
+            batch = []
+            batch_id = 0
+            while True:
+                try:
                     data = await queue.get()
                     if data is None:
                         if batch:
-                            await self.insert_batch_with_retry(cursor, batch)
-                        self.logging.info(f"Consumer-{consumer_id} exiting")
+                            await self.insert_batch_with_retry(conn, query, batch, batch_id, consumer_id)
+                            batch_id += 1
+                        self.logger.info(f"Consumer-{consumer_id} exiting")
                         break
 
                     batch.append(data)
                     if len(batch) >= batch_size:
-                        await self.insert_batch_with_retry(cursor, batch)
+                        await self.insert_batch_with_retry(conn, query, batch, batch_id, consumer_id)
+                        batch_id += 1
                         batch.clear()
-
+                finally:
                     queue.task_done()
 
-    async def producer(self, queue, items):
+    async def producer(self, queue, items, num_consumers):
         """Produces items data and adds it to the queue."""
-        for item in range(items):
+        for item in items:
             await queue.put(item)
-            self.logging.info(f"Produced: {str(item)}")
+            self.logger.info(f"Produced: {str(item)}")
 
         # Signal consumers to stop
-        for _ in range(len(items)):
+        for _ in range(num_consumers):
             await queue.put(None)
 
     async def create_db_pool(self):
         """Create a connection pool for Postgres."""
         return await asyncpg.create_pool(**self.settings.POSTGRES_CONFIG)
 
-    async def insert(self, query, items, num_consumers = 5, batch_size = 5):
+    async def insert(self, query, items, num_consumers = 2, batch_size = 2):
         queue = asyncio.Queue()
         pool = await self.create_db_pool()
 
-        producer_task = asyncio.create_task(self.producer(queue, items))
-        consumers = [asyncio.create_task(self.consumer(queue, pool, query, consumer_id, batch_size)) for consumer_id in range(num_consumers)]
+        try:
+            producer_task = asyncio.create_task(self.producer(queue, items, num_consumers))
+            consumers = [asyncio.create_task(self.consumer(queue, pool, query, consumer_id, batch_size)) for consumer_id in range(num_consumers)]
 
-        await producer_task                 # Ensure all data is produced
-        await queue.join()                  # Wait until all tasks are processed
-        await asyncio.gather(*consumers)    # Ensure all consumers exit
-        await pool.close()                  # Ensure all database connection closed 
+            await producer_task                                         # Ensure all data is produced
+            await queue.join()                                          # Wait until all tasks are processed
+            await asyncio.gather(*consumers, return_exceptions=True)    # Ensure all consumers exit
+        finally:
+            await pool.close()
--- docker/Flower.Dockerfile
+++ docker/Flower.Dockerfile
@@ -1,6 +1,9 @@
 # Use the official Flower image as a base
 FROM mher/flower:latest
 
+# Set environment variable to enable unauthenticated API
+ENV FLOWER_UNAUTHENTICATED_API=true
+
 # Optional: expose the port
 EXPOSE 5555
 
